e79922b77c4689bd1321a13406e769c3e4e91e98
predohr <pedro.henrique9760@icloud.com>
2024-05-14 19:08:48 -0300

    initial version

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal expenses'),
+      ),
+      body: Center(
+        child: Text('Inicial version'),
+      ),
+    );
+  }
+}
